e8d59a55a794570529c98b459f94d4bb1381e37e
SeanKudrna <62570862+SeanKudrna@users.noreply.github.com>
2024-07-23 20:45:56 -0500

    Improved search dropdown
    #17
    - Movie poster now displayed in search dropdown along with release date, rating out of 10, and number of reviews.
    - Search is improved by sorting the searches first by closest match to current query in search bar, then next by popularity. Most popular movies matching the search will appear first, and less popular movies matching the search will be further down the list.

--- src/components/images/AppLogo.tsx
+++ src/components/images/AppLogo.tsx
@@ -1,28 +1,29 @@
-import React from 'react';
-import {Image, View} from 'react-native';
+import React from "react";
+import { Image } from "react-native";
 
-// Import Custom Styles
-import { myStyles } from '@/src/utils/constants/styles';
+// Import custom styles
+import { myStyles } from "@/src/utils/constants/styles";
 
 // Import images
-import logowhite from '@/src/assets/images/logo/mainlogo_512_white.png'
-import logoblack from '@/src/assets/images/logo/mainlogo_512_black.png'
+// @ts-ignore
+import logowhite from "@/src/assets/images/logo/mainlogo_512_white.png";
+// @ts-ignore
+import logoblack from "@/src/assets/images/logo/mainlogo_512_black.png";
 
-// interface Props {
-//   pressed: () => void,
-//   text: string,
-// }
-
+/**
+ * A functional component that renders the application logo in light mode.
+ *
+ * @returns {JSX.Element} - The rendered light mode logo.
+ */
 export const AppLogoLightMode: React.FC = () => {
-  return (
-    <Image style={myStyles.logo} source={logoblack} />
-  )
+  return <Image style={myStyles.logo} source={logoblack} />;
 };
 
+/**
+ * A functional component that renders the application logo in dark mode.
+ *
+ * @returns {JSX.Element} - The rendered dark mode logo.
+ */
 export const AppLogoDarkMode: React.FC = () => {
-  return (
-    <Image style={myStyles.logo} source={logowhite} />
-  )
+  return <Image style={myStyles.logo} source={logowhite} />;
 };
-
-
