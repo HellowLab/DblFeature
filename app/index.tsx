46c00fa23286a371d48e32a7f4db1d95a06b3c7e
Kevin Mengarelli <mango23322@gmail.com>
2024-08-11 22:13:29 -0500

    add login persistance

--- app/index.tsx
+++ app/index.tsx
@@ -1,14 +1,45 @@
-import { Redirect } from "expo-router";
-import React from "react";
+import React,  { useEffect } from "react";
+import { router } from "expo-router";
+
+import { getToken } from "@/src/utils/store/TokenStore";
+import { View } from "react-native";
+import { AppLogo } from "@/src/components/images/AppLogo";
+import Loader from "@/src/components/loaders/Loader";
 
 // This can be used as the splash screen to perform any api calls or other async tasks
 // and then redirect to the login screen or any other screen if the user is already logged in.
 const Index = () => {
-  // since we don't have any async tasks to perform, we can just redirect to the login screen
-  // once we have user auth, we can add the logic here to check if the user is logged in and redirect accordingly
-  return <Redirect href="/(login)" />;
 
-  // for testing purposes, we can ignore the login screen and redirect to the drawer directly
-  // return <Redirect href="/(drawer)" />;
+  /**
+   * Check if the user is logged in by checking if a token exists in the store
+   * @returns Redirects to the login screen if no token is found, otherwise redirects to the home screen
+   */
+  const isLoggedIn = async () => {
+    try {
+      const token = await getToken();
+      if (token) {
+        console.log("token: ", token)
+        router.replace("/(drawer)");
+      }
+      else {
+        console.log("no token")
+        router.replace("/(login)");
+      }
+    } catch (error) {
+      console.error(error);
+    } 
+  };
+
+  useEffect(() => {
+    isLoggedIn(); // Check for stored token when the component mounts
+  }, []);
+  
+  // Display the app logo and a loading spinner while the user's login status is checked -- acts as a splash screen
+  return (
+    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+      <AppLogo />
+      <Loader />
+    </View>
+  );
 };
 export default Index;
